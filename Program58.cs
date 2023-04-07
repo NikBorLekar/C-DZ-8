0493cf9a9a8160fc622e56156aa188960108fab0
Nik_Bor <126783560+NikBorLekar@users.noreply.github.com>
2023-04-07 18:21:38 +0700

    Add files via upload

---
+++ Program58.cs
@@ -0,0 +1,68 @@
+﻿
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] Martrix1 = new int[m, n];
+Array1(Martrix1);
+Console.WriteLine($"\nПервая матрица:");
+Array2(Martrix1);
+
+int[,] Martrix2 = new int[n, p];
+Array1(Martrix2);
+Console.WriteLine($"\nВторая матрица:");
+Array2(Martrix2);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(Martrix1, Martrix2, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+Array2(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void Array1(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void Array2 (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
